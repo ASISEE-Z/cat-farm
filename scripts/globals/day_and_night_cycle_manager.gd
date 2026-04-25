0f6e622a112b094e350446bf5f3de1a24bb91a8f
ASISEE-Z <3521160437@qq.com>
2026-04-26 00:10:31 +0800

    2026.4.26

--- scripts/globals/day_and_night_cycle_manager.gd
+++ scripts/globals/day_and_night_cycle_manager.gd
@@ -14,7 +14,7 @@ var initial_minute: int = 30
 
 var time: float = 0.0
 var current_minute: int = -1
-var current_daya: int = 0
+var current_day: int = 0
 
 
 signal game_time(time: float)
@@ -22,5 +22,34 @@ signal time_tick(day: int, hour: int, minute: int)
 signal time_tick_day(day: int)
 
 
-func set_intial_time() -> void:
+func _ready() -> void:
+	set_initial_time()
+
+
+func _process(delta: float) -> void:
+	time += delta * game_speed * GAME_MINUTE_DURATION
+	game_time.emit(time)
+
+	recalculate_time()
+
+
+func set_initial_time() -> void:
 	var initial_total_minutes = initial_day * MINUTES_PER_DAY + (initial_hour * MINUTES_PER_HOUR) + initial_minute
+
+	time = initial_total_minutes * GAME_MINUTE_DURATION
+
+
+func recalculate_time() -> void:
+	var total_minutes: int = int(time / GAME_MINUTE_DURATION)
+	var day: int = int(total_minutes / MINUTES_PER_DAY)
+	var current_day_minutes: int = total_minutes % MINUTES_PER_DAY
+	var hour: int = int(current_day_minutes / MINUTES_PER_HOUR)
+	var minute: int = int(current_day_minutes % MINUTES_PER_HOUR)
+
+	if current_minute != minute:
+		current_minute = minute
+		time_tick.emit(day, hour, minute)
+
+	if current_day != day:
+		current_day = day
+		time_tick_day.emit(day)
